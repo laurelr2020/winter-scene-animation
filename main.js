5c42caf8b231b12ecedd67dfa594d4a99cfa40ec
laurelr2020 <reganlaurell@gmail.com>
2019-11-28 08:54:42 -0500

    remove animation

--- main.js
+++ main.js
@@ -14,36 +14,3 @@ function drawBackground(){
 	global.background.drawSky();
 	global.background.drawSun();
 }	
-
-function animateScene(){
-	if(global.running == false){
-		global.running = true;
-		global.moveCount = 0;
-		requestAnimationFrame(moveCloudsIn);
-	}
-}
-
-function moveCloudsIn(){
-	global.moveCount ++;
-	global.context.save();
-
-	if(global.moveCount < 120){
-		global.snowCloudLeft.clear();
-		global.snowCloudLeft.move(1.2, 1.2); 
-		// global.snowCloudMiddle.move(32, 6);
-		// global.snowCloudRight.move(-22, 7);
-		global.context.restore();
-		requestAnimationFrame(moveCloudsIn);
-	}
-}
-
-function smallMoveOver(){
-	global.moveCount ++;
-	drawAll();
-
-	if(global.moveCount < 20){
-		requestAnimationFrame(smallMoveOver);
-	}else{
-		global.running = false;
-	}
-}
