aee4e801982effc5d7f5a9da9fd1a6216780a964
laurelr2020 <reganlaurell@gmail.com>
2019-11-29 14:19:47 -0500

    animate snow man

--- main.js
+++ main.js
@@ -8,27 +8,6 @@ function init(){
 	drawBackground();
 	global.snowstorm.draw();
 	global.drawClouds();
-
-	global.snowman.drawBottom();
-	global.snowman.drawMiddle();
-	global.snowman.drawTop();
-
-	global.snowman.drawBottomCoal();
-	global.snowman.drawMiddleCoal();
-	global.snowman.drawTopCoal();
-	global.snowman.drawRightEye();
-	global.snowman.drawLeftEye();
-
-	global.snowman.drawMouthLeft();
-	global.snowman.drawMouthMiddleLeft();
-	global.snowman.drawMouthMiddle();
-	global.snowman.drawMouthMiddleRight();
-	global.snowman.drawMouthRight();
-
-	global.snowman.drawNose();
-
-	global.snowman.drawLeftArm();
-	global.snowman.drawRightArm();
 }
 
 function drawBackground(){
@@ -62,7 +41,61 @@ function smallMoveOver(){
 			}
 			global.context.restore();
 			global.drawClouds();
-
+			requestAnimationFrame(smallMoveOver);
+		}else{
+			requestAnimationFrame(smallMoveOver);
+		}
+	}else if(global.moveCount == 275){
+		global.background.drawSnowGround();
+		requestAnimationFrame(smallMoveOver);
+	}else if(global.moveCount > 275){
+		if(global.moveCount == 280){
+			global.snowman.drawBottom();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 290){
+			global.snowman.drawMiddle();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 300){
+			global.snowman.drawTop();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 310){
+			global.snowman.drawBottomCoal();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 320){
+			global.snowman.drawMiddleCoal();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 330){
+			global.snowman.drawTopCoal();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 340){
+			global.snowman.drawMouthLeft();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 350){
+			global.snowman.drawMouthMiddleLeft();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 360){
+			global.snowman.drawMouthMiddle();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 370){
+			global.snowman.drawMouthMiddleRight();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 380){
+			global.snowman.drawMouthRight();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 390){
+			global.snowman.drawLeftEye();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 400){
+			global.snowman.drawRightEye();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 410){
+			global.snowman.drawNose();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 420){
+			global.snowman.drawLeftArm();
+			requestAnimationFrame(smallMoveOver);
+		}else if(global.moveCount == 430){
+			global.snowman.drawRightArm();
 			requestAnimationFrame(smallMoveOver);
 		}else{
 			requestAnimationFrame(smallMoveOver);
@@ -70,7 +103,6 @@ function smallMoveOver(){
 	}else{
 		global.running = false;
 		global.moveCount = 0;
-		global.background.drawSnowGround();
 	}
 }
 
