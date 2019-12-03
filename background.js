4cb1f96e33075d0c7b13904421142294ed17c297
laurelr2020 <reganlaurell@gmail.com>
2019-12-03 14:16:38 -0500

    add referece to snow cloud. remove unneeded things

--- background.js
+++ background.js
@@ -12,18 +12,8 @@ function Background(){
         global.context.fillRect(0, 300, 800, 200);
     }
 
-    this.drawDaySky = function drawDaySky(){
-        global.context.fillStyle = "skyblue";
-        global.context.fillRect(0, 0, 800, 300);
-    }
-
     this.drawStormSky = function drawStormSky(){
         global.context.fillStyle = "#1f49c7";
         global.context.fillRect(0, 0, 800, 300);
-    }
-    
-    this.drawSun = function drawSun(){
-        let image = document.getElementById("sun");
-        global.context.drawImage(image, 650, 10);
     }
 }
